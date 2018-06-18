160bbb3683e89f88569dcbf29e4dbdf581de371f
annaliner <annaliner89@gmail.com>
2018-06-18 21:17:44 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,11 @@
-guest_name=
+guest_name= "Hermione" 
+party_name= "Best Halloween Party Ever"
+date= "October 31st"
+time= "6 pm"
+host_name= "Harry Potter"
+
+puts "Dear #{guest_name},
+You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
+Sincerely,
+
+#{host_name}"
